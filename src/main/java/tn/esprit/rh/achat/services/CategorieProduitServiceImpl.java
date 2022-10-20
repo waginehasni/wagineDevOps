9e88d612c54935c8e8e6dc16907898da32a79502
waginehasni <wagine.hasni@esprit.tn>
2022-10-20 16:25:36 +0100

    First commit

--- src/main/java/tn/esprit/rh/achat/services/CategorieProduitServiceImpl.java
+++ src/main/java/tn/esprit/rh/achat/services/CategorieProduitServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Service
 public class CategorieProduitServiceImpl implements ICategorieProduitService {
 
-	
+//test	
 	@Autowired
 	CategorieProduitRepository categorieProduitRepository;
 	@Override
